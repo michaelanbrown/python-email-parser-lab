28da712dc1b681fd1083085d6d5779895780ba64
michaelanbrown <m_brownbrown@yahoo.com>
2024-10-04 13:08:34 -0400

    initializing an email regex

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -2,8 +2,9 @@ import re
 
 class EmailAddressParser:
 
+    email_regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
     def __init__(self, emails):
         self.emails = emails
 
     def parse(self):
-        
